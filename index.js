a7a8b4d827908c1e867f618a3a34530d60014a1a
Jose <Sena@ARMAPRCCIFOD560.FormacionSena.RED>
2024-02-22 08:32:06 -0500

    Funcion para restar

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 function sumar(num1, num2) {
   return num1 + num2;
 }
+//Función para restar dos numeros
+function restar(num1, num2) {
+    return num1 - num2;
+}
 
 // Función principal del programa
 function calculadora() {
